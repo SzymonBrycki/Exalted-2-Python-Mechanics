7a839421556bbaf6a2f7335c8bff6475f7bf08c9
Szymon Brycki <szymon.brycki@gmail.com>
2024-07-29 18:56:16 +0200

    Add files via upload

--- Code/AttributesOrSkills.py
+++ Code/AttributesOrSkills.py
@@ -16,14 +16,13 @@ class AttributeOrSkill:
 
 class Attribute(AttributeOrSkill):
     def __init__(self):
+        super().__init__()
         self.minVal = 1
 
         '''
         AttributeNames = "Strength Dexterity Stamina Charisma Manipulation Appearance Perception Intelligence Wits"
         self.AttributesNamesList = AttributeNames.split()
         '''
-        
-        super().__init__()
     
     '''
     def attributeCategory(self, nameOfAttribute):
@@ -37,19 +36,253 @@ class Attribute(AttributeOrSkill):
             raise ValueError
     '''
 
+class Skill(AttributeOrSkill):
+    def __init__(self):
+        super().__init__()
+        self.minVal = 0
+
+#################
+#
+# ATTRIBUTES
+#
+#################
+
 class Strength(Attribute):
     def __init__(self):
+        super().__init__()
+        self.attVal = self.minVal
         self.name = "Strength"
         self.attributeCategory = "Physical"
 
-        super().__init__()
-        
-strength = Strength()
-print(strength.name, strength.minVal, strength.maxVal, strength.attributeCategory)
 
+class Dexterity(Attribute):
+    def __init__(self):
+        super().__init__()
+        self.attVal = self.minVal
+        self.name = "Dexterity"
+        self.attributeCategory = "Physical"
+
+class Stamina(Attribute):
+    def __init__(self):
+        super().__init__()
+        self.attVal = self.minVal
+        self.name = "Stamina"
+        self.attributeCategory = "Physical"
+
+class Charisma(Attribute):
+    def __init__(self):
+        super().__init__()
+        self.attVal = self.minVal
+        self.name = "Charisma"
+        self.attributeCategory = "Social"
+
+class Manipulation(Attribute):
+    def __init__(self):
+        super().__init__()
+        self.attVal = self.minVal
+        self.name = "Manipulation"
+        self.attributeCategory = "Social"
+
+class Appearance(Attribute):
+    def __init__(self):
+        super().__init__()
+        self.attVal = self.minVal
+        self.name = "Appearance"
+        self.attributeCategory = "Social"
+
+class Perception(Attribute):
+    def __init__(self):
+        super().__init__()
+        self.attVal = self.minVal
+        self.name = "Perception"
+        self.attributeCategory = "Mental"
+
+class Intelligence(Attribute):
+    def __init__(self):
+        super().__init__()
+        self.attVal = self.minVal
+        self.name = "Intelligence"
+        self.attributeCategory = "Mental"
+
+class Wits(Attribute):
+    def __init__(self):
+        super().__init__()
+        self.attVal = self.minVal
+        self.name = "Wits"
+        self.attributeCategory = "Mental"
+
+#################
+#
+# SKILLS
+#
+#################
+
+# Dawn
+
+class Archery(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Archery"
+
+class MartialArts(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Martial Arts"
+
+class Melee(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Melee"
+
+class Thrown(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Thrown"
+
+class War(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "War"
+
+# Zenith
+
+class Integrity(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Integrity"
+
+class Performance(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Performance"
+
+class Presence(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Presence"
+
+class Resistance(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Resistance"
+
+class Survival(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Survival"
+
+# Twilight
+
+class Craft(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Craft"
+
+class Investigation(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Investigation"
+
+class Lore(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Lore"
+
+class Medicine(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Medicine"
+
+class Occult(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Occult"
+
+# Night
+
+class Athletics(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Athletics"
+
+class Awareness(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Awareness"
+
+class Dodge(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Dodge"
+
+class Larceny(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Larceny"
+
+class Stealth(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Stealth"
+
+# Eclipse
+
+class Bureaucracy(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Bureaucracy"
+
+class Linguistics(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Linguistics"
+
+class Ride(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Ride"
+
+class Sail(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Sail"
+
+class Socialize(Skill):
+    def __init__(self):
+        super().__init__()
+        self.skillVal = self.minVal
+        self.name = "Socialize"
+
+strength = Strength()
+print(strength.name, strength.minVal, strength.maxVal, strength.attributeCategory, strength.attVal)
+
+socialize = Socialize()
+print(socialize.name, socialize.minVal, socialize.maxVal, socialize.skillVal)
     
 # AttributesList = list()
-
-
 
 # print(type(AttributesNamesList))
